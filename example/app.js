92fa667726f82d1f07d3eeaa806184836f6aafe2
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-06-15 11:05:14 +0200

    Add test case and support for handling Zod errors as client errors instead of server errors.

--- example/app.js
+++ example/app.js
@@ -1,4 +1,5 @@
 import express from '../index.js';
+import z from 'zod';
 
 const app = await express();
 
@@ -12,6 +13,15 @@ app.get('/error', (req, res, next) => {
     er.additional = "information";
     console.log("er", er.constructor.name, er.name);
     next(er);
+});
+
+const validationRequest = z.object({
+    message: z.string(),
+    numerically: z.number().positive().gt(5).int().optional(),
+});
+app.post('/validation', express.json(), (req, res) => {
+    const body = validationRequest.parse(req.body);
+    res.send(body);
 });
 
 app.listen(5212);
